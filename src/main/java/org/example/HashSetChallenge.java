ca562cbc09d0e739aa44c0175ab6d9f98a963af4
Conor O'Brien <32160798+Con-OBrien@users.noreply.github.com>
2023-07-17 10:01:24 +0100

    HashSet completed comments

--- src/main/java/org/example/HashSetChallenge.java
+++ src/main/java/org/example/HashSetChallenge.java
@@ -6,10 +6,13 @@ import java.util.Scanner;
 public class HashSetChallenge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        //Input - First line = integer denoting # of pairs
         int n = scanner.nextInt();
         HashSet<String> set = new HashSet<>();
 
+        //each next pair is marked by first and second
+        //if our set doesn't contain the pair, then we add it
+        //at the end of each pair, prints how many unique pairs there are
         for (int i = 0; i < n; i++) {
             String first = scanner.next();
             String second = scanner.next();
